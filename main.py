596ddbc40211fb934d1f4be120586c1ccbf3d3c3
Serhii <casiomania@proton.me>
2024-03-20 22:31:47 +0200

    pep8 fix

--- main.py
+++ main.py
@@ -10,3 +10,37 @@
 # No more; and by a sleep to say we end The heart-ache and the thousand natural shocks
 # That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep
 
+with open('text.txt', 'r', encoding='utf-8') as input_file:
+    content = input_file.read()
+    words = content.split()
+    long_words = [word.strip(',.') for word in words if len(word) >= 7]
+
+with open('new.txt', 'w', encoding='utf-8') as output_file:
+    output_file.write(' '.join(long_words))
+
+with open('new.txt', 'r', encoding='utf-8') as output_file:
+    print("Task 1")
+    print("Edited text:")
+    print(output_file.read())
+
+with open('text.txt', 'r', encoding='utf-8') as input_file:
+    content = input_file.read()
+    words = content.split()
+    word_count = len(words)
+    print("\nTask 2")
+    print("Words count in file:", word_count)
+
+with open('text.txt', 'r', encoding='utf-8') as input_file:
+    content = input_file.read()
+    prohibited_words = ['die']
+    replaced_text = content
+    count = 0
+    for word in prohibited_words:
+        replaced_text, n = replaced_text.replace(word, '*' * len(word)), replaced_text.count(word)
+        count += n
+
+    print("\nTask 3")
+    print("Prohibited word:", prohibited_words[0])
+    print("Result:")
+    print(replaced_text)
+    print("Statistics: {} word replacements {}.".format(count, prohibited_words[0]))
