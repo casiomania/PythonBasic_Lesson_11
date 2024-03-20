37a1e71055838683661a3e61aac605f44f2881f0
Serhii <casiomania@proton.me>
2024-03-20 22:27:48 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 1. Given a text file. You need to create a new file to rewrite all the words consisting of at least seven letters from the first file.
+# 2. Given a text file. Count the number of words in it.
+# 3. Create a program that checks the text for invalid words.
+# If an invalid word is found, it should be replaced with a set of characters *.
+# Based on the results of the program, you need to show the statistics of actions.
+# We have text:
+# To be, or not to be, that is the question,
+# Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune,
+# Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep;
+# No more; and by a sleep to say we end The heart-ache and the thousand natural shocks
+# That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep
+
